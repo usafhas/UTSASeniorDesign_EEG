a87ceb1ad425c5c9ad6ac894089749c1b31f817b
usafhas <xzy385@my.utsa.edu>
2016-12-29 18:08:44 -0600

    Feature Dimensions
    added feature dimensions into csv output

--- machinelearning_Acc_ExperimentSvC.py
+++ machinelearning_Acc_ExperimentSvC.py
@@ -30,13 +30,13 @@ ff=[]
 for bbb in range(0,np.size(windows)):
 
     WS = windows[bbb] # window size variable
-	
+    
 #    s1_happy = np.load('./Data/Training/happyW{0}.npy'.format(WS))
     s1_stress = np.load('./Data/Experiments/stressW{0}.npy'.format(WS))
     s1_norm = np.load('./Data/Experiments/normW{0}.npy'.format(WS))
     s1_calm = np.load('./Data/Experiments/calmW{0}.npy'.format(WS))
-	
-	
+    
+    
     x = np.size(s1_norm)
     y = np.size(s1_calm)
     z = np.size(s1_stress)
@@ -50,108 +50,113 @@ for bbb in range(0,np.size(windows)):
     q=0
     """ ------------0--------------1-----------------2-------------3-----------4-----------5--------------6-------------7-----------------8----------------9----------10-----------11------------12----------13-----------14"""
     for p in range(0,6):
-		if p ==0:
-			clf = KNeighborsClassifier(n_neighbors=5) 
-			print "=====================using KNN = 5================================="
-			cs = "using KNN = 5"
-		elif p ==1:
-			clf = KNeighborsClassifier(n_neighbors=3) 
-			print "============================using KNN = 3========================="
-			cs = "using KNN = 3"
-		elif p==2:
-			clf = KNeighborsClassifier(n_neighbors=7)
-			print "================================using KNN = 7========================="
-			cs = "using KNN = 7"
-		elif p==3:
-			clf = svm.SVC(kernel = 'rbf', C =3.0, decision_function_shape='ovr')
-			print "===============================using Gaussian============================="
-			cs = "using Gaussian"
-		elif p ==4:
-			clf = naive_bayes.GaussianNB()
-			print "=====================Naive Bays Gaussian==============================="
-			cs = "Naive Bayes Gaussian"
-		elif p ==5:
-			clf = QuadraticDiscriminantAnalysis()
-			print "=====================Quadratic Discriminant Analysis==============================="
-			cs = "Quadratic Discriminant Analysis"
-		else:
-			clf = svm.SVC(kernel = 'poly', C =1.0, degree = 5, decision_function_shape='ovr')
-			print "=====================using Poly degree = 3==============================="
-			cs = "using Poly degree = 5"
-			
-		""" -------------------Features---------------------------------------------------------------------------"""
-		for q in range(0,np.size(feature)):
-#			if (p ==4 or p==5 or p==6) and q == 4:
-#				q+=1
-#			if (p ==4 or p==5 or p==6) and q ==5:
-#				q+=1
-#			if (p ==4 or p==5 or p==6) and q == 12:
-#				q+=1
-#			if (p ==4 or p==5 or p==6) and q ==13:
-#				q+=1
-#				break
-				
-			print "feature %s" %feature[q]
-	
-			for i in range(0,x):
-				n["norm{0}".format(i)]=np.load(s1_norm[i]+feature[q])
-			# Get Sad
-			for j in range(0,y):
-				c["calm{0}".format(j)]=np.load(s1_calm[j]+feature[q])
-				
-			for k in range(0,z):
-				a["angry{0}".format(k)] = np.load(s1_stress[k]+feature[q])
-			
-			"""-----------------Stack the Data ---------------------------------------------"""
-			
-			train = np.vstack((a['angry0'],a['angry1']))
-			
-			for k in range(2,z):
-				train = np.vstack((train,a["angry{0}".format(k)]))
-			htrain,zz=np.shape(train)
-			yh =np.ones(htrain)
-			
-			#        
-			for m in range(0,y):
-				train = np.vstack((train,c["calm{0}".format(m)]))
-			atrain,zz=np.shape(train)
-			#        ya = np.ones(atrain-strain)*2
-			ya = np.ones(atrain-htrain)*2
-			#
+        if p ==0:
+            clf = KNeighborsClassifier(n_neighbors=5) 
+            print "=====================using KNN = 5================================="
+            cs = "using KNN = 5"
+        elif p ==1:
+            clf = KNeighborsClassifier(n_neighbors=3) 
+            print "============================using KNN = 3========================="
+            cs = "using KNN = 3"
+        elif p==2:
+            clf = KNeighborsClassifier(n_neighbors=7)
+            print "================================using KNN = 7========================="
+            cs = "using KNN = 7"
+        elif p==3:
+            clf = svm.SVC(kernel = 'rbf', C =3.0, decision_function_shape='ovr')
+            print "===============================using Gaussian============================="
+            cs = "using Gaussian"
+        elif p ==4:
+            clf = naive_bayes.GaussianNB()
+            print "=====================Naive Bays Gaussian==============================="
+            cs = "Naive Bayes Gaussian"
+        elif p ==5:
+            clf = QuadraticDiscriminantAnalysis()
+            print "=====================Quadratic Discriminant Analysis==============================="
+            cs = "Quadratic Discriminant Analysis"
+        else:
+            clf = svm.SVC(kernel = 'poly', C =1.0, degree = 5, decision_function_shape='ovr')
+            print "=====================using Poly degree = 3==============================="
+            cs = "using Poly degree = 5"
+            
+        """ -------------------Features---------------------------------------------------------------------------"""
+        for q in range(0,np.size(feature)):
+#            if (p ==4 or p==5 or p==6) and q == 4:
+#                q+=1
+#            if (p ==4 or p==5 or p==6) and q ==5:
+#                q+=1
+#            if (p ==4 or p==5 or p==6) and q == 12:
+#                q+=1
+#            if (p ==4 or p==5 or p==6) and q ==13:
+#                q+=1
+#                break
+                
+            print "feature %s" %feature[q]
+    
+            for i in range(0,x):
+                n["norm{0}".format(i)]=np.load(s1_norm[i]+feature[q])
+            # Get Sad
+            for j in range(0,y):
+                c["calm{0}".format(j)]=np.load(s1_calm[j]+feature[q])
+                
+            for k in range(0,z):
+                a["angry{0}".format(k)] = np.load(s1_stress[k]+feature[q])
+            
+            sizefx= np.shape(np.load(s1_norm[0]+feature[q]))
+            
+            """-----------------Stack the Data ---------------------------------------------"""
+            
+            train = np.vstack((a['angry0'],a['angry1']))
+            
+            for k in range(2,z):
+                train = np.vstack((train,a["angry{0}".format(k)]))
+            htrain,zz=np.shape(train)
+            yh =np.ones(htrain)
+            
+            #        
+            for m in range(0,y):
+                train = np.vstack((train,c["calm{0}".format(m)]))
+            atrain,zz=np.shape(train)
+            #        ya = np.ones(atrain-strain)*2
+            ya = np.ones(atrain-htrain)*2
+            #
 
-			train = np.array(train)
-			train.reshape(-1,1)
-			
-			
-			
-			yl = np.append(yh,ya)
-			yl.reshape(-1,1)
-			
-			
-			X_train, X_test, Y_train, Y_test = train_test_split(train, yl, test_size=0.33, random_state=42)
-			
-			clf.fit(X_train,Y_train)
-			
-			
-			accuracy = 0.0
-			
-			result = clf.predict(X_test)
-			
-			for m in range(0,np.size(result)):
-			#        print 'Sample Lable', Y_test[m] ,'sample prediction', s[m]
-				if Y_test[m] == result[m]:
-					accuracy = accuracy +1.0
-					
-			accuracy = accuracy/np.size(result)*100
-			print accuracy
-			if accuracy >= 80:
-				acstr = "\r\n%s \t\t\t %s \t\t \\b %d \\b0\r\n" %(cs, feature[q], accuracy)
-			else:
-				acstr = "\r\n%s \t\t\t %s \t\t %d \r\n" %(cs, feature[q], accuracy)
-			print acstr
-			acc.append(acstr)
-			csva.append(['\t',cs, '\t',feature[q],'\t', accuracy,'\t',hvan,'\t',WS,'\t'])
-			
+            train = np.array(train)
+            train.reshape(-1,1)
+            
+            
+            
+            yl = np.append(yh,ya)
+            yl.reshape(-1,1)
+            
+            
+            X_train, X_test, Y_train, Y_test = train_test_split(train, yl, test_size=0.33, random_state=42)
+            
+            clf.fit(X_train,Y_train)
+            
+            
+            accuracy = 0.0
+            
+            result = clf.predict(X_test)
+            
+            for m in range(0,np.size(result)):
+            #        print 'Sample Lable', Y_test[m] ,'sample prediction', s[m]
+                if Y_test[m] == result[m]:
+                    accuracy = accuracy +1.0
+                    
+            accuracy = accuracy/np.size(result)*100
+            print accuracy
+            if accuracy >= 80:
+                acstr = "\r\n%s \t\t\t %s \t\t \\b %d \\b0\r\n" %(cs, feature[q], accuracy)
+            else:
+                acstr = "\r\n%s \t\t\t %s \t\t %d \r\n" %(cs, feature[q], accuracy)
+            print acstr
+            acc.append(acstr)
+            if np.size(sizefx) ==2:
+                  csva.append(['\t',cs, '\t',feature[q],'\t', accuracy,'\t',hvan,'\t',WS,'\t', "x=",sizefx[0]," y=",sizefx[1], '\t'])
+            else:
+                  csva.append(['\t',cs, '\t',feature[q],'\t', accuracy,'\t',hvan,'\t',WS,'\t', "x=",1, "y=",sizefx[0], '\t'])
+            
 
 """========================= Train Classifier Fully =========================="""
 #clf.fit(train,yl)
@@ -163,7 +168,7 @@ with open('accuracy.rtf', 'w') as f:
     f.writelines(acc)
     f.write(r'}\n\x00')
 
-with open('Accuracy_testing.csv', 'wb') as myfile:
+with open('Accuracy_testing_experiment.csv', 'wb') as myfile:
     wr = csv.writer(myfile, delimiter = '\t', quoting=csv.QUOTE_NONE)
     x,y = np.shape(csva)
     for c in range (0,x):
